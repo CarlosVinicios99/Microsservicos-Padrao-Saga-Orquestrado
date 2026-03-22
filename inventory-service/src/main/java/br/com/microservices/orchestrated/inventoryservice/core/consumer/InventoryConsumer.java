889d40a1e4992ea19610c1eb604788c34fbcf477
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2026-03-22 17:54:11 -0300

    feature: criação dos consumers para o inventory-service

---
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -0,0 +1,48 @@
+package br.com.microservices.orchestrated.inventoryservice.core.consumer;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
+
+@Component
+public class InventoryConsumer {
+	
+	private final Logger logger = Logger.getLogger(InventoryConsumer.class.getName());
+	
+	private JsonUtil jsonUtil;
+	
+	public InventoryConsumer(JsonUtil jsonUtil) {
+		this.jsonUtil = jsonUtil;
+	}
+	
+	
+	@KafkaListener(
+		groupId = "${spring.kafka.consumer.group-id}",
+		topics = "${spring.kafka.topic.inventory-success}"
+	)
+	public void consumeSuccessEventTopic(String payload) {
+		this.logger.log(
+			Level.INFO, "Receiving success event from inventory-success topic " 
+			+ payload
+		);
+		var event = this.jsonUtil.toEvent(payload);
+		this.logger.log(Level.INFO, event.toString());
+	}
+	
+	@KafkaListener(
+		groupId = "${spring.kafka.consumer.group-id}",
+		topics = "${spring.kafka.topic.inventory-fail}"
+	)
+	public void consumeFailEventTopic(String payload) {
+		this.logger.log(
+			Level.INFO, "Receiving rollback event from inventory-fail topic " 
+			+ payload
+		);
+		var event = this.jsonUtil.toEvent(payload);
+		this.logger.log(Level.INFO, event.toString());
+	}
+}
