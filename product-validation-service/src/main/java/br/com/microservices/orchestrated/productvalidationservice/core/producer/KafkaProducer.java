897d8352e7413b5cdff889ed1e47f79648da8ebd
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2026-03-22 14:35:26 -0300

    feature: criando producer para o tópico orchestrator no product validation service

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/producer/KafkaProducer.java
@@ -0,0 +1,40 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.producer;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+
+
+public class KafkaProducer {
+	
+	private KafkaTemplate<String, String> kafkaTemplate;
+	
+	private Logger logger = Logger.getLogger(KafkaProducer.class.getName());
+	
+	@Value("${spring.kafka.topic.orchestrator}")
+	private String orchestratorTopic;
+	
+	
+	public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+		this.kafkaTemplate = kafkaTemplate;
+	}
+	
+	
+	public void sendEvent(String payload) {
+		try {
+			this.logger.log(
+				Level.SEVERE, "Sending event to topic " + orchestratorTopic + 
+				" with data " + payload
+			);
+			this.kafkaTemplate.send(orchestratorTopic, payload);
+		}
+		catch(Exception error) {
+			this.logger.log(
+				Level.SEVERE, "Error trying to send data to topic " + orchestratorTopic + 
+				" " + "with data " + payload
+			);
+		}
+	}
+}
