b6ddc36f7f0d36cfe80186cf5868cd635cc04f3a
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2026-03-22 12:45:46 -0300

    feature: criação de utilitário para conversões de json para string e eventos nos serviços

---
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/utils/JsonUtil.java
@@ -0,0 +1,37 @@
+package br.com.microservices.orchestrated.orderservice.core.utils;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.microservices.orchestrated.orderservice.core.document.Event;
+
+@Component
+public class JsonUtil {
+	
+	private final ObjectMapper objectMapper;
+	
+	public JsonUtil(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+	
+	
+	public String toJson(Object object) {
+		try {
+			return this.objectMapper.writeValueAsString(object);
+		}
+		catch(Exception error) {
+			return "";
+		}
+	}
+	
+	public Event toEvent(String json) {
+		try {
+			return this.objectMapper.readValue(json, Event.class);
+		}
+		catch(Exception error) {
+			return null;
+		}
+	}
+	
+}
---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/utils/JsonUtil.java
@@ -0,0 +1,37 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.utils;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.microservices.orchestrated.productvalidationservice.core.dto.Event;
+
+@Component
+public class JsonUtil {
+	
+	private final ObjectMapper objectMapper;
+	
+	public JsonUtil(ObjectMapper objectMapper) {
+		this.objectMapper = objectMapper;
+	}
+	
+	
+	public String toJson(Object object) {
+		try {
+			return this.objectMapper.writeValueAsString(object);
+		}
+		catch(Exception error) {
+			return "";
+		}
+	}
+	
+	public Event toEvent(String json) {
+		try {
+			return this.objectMapper.readValue(json, Event.class);
+		}
+		catch(Exception error) {
+			return null;
+		}
+	}
+	
+}
