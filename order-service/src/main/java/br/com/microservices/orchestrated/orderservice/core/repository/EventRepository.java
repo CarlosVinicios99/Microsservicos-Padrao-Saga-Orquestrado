223322781a9c281d6e7b6fd051db1c693dccd0c6
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2026-03-27 20:31:44 -0300

    feature: desenvolvendo rotas para buscar eventos

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/repository/EventRepository.java
@@ -1,5 +1,8 @@
 package br.com.microservices.orchestrated.orderservice.core.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +11,9 @@ import br.com.microservices.orchestrated.orderservice.core.document.Event;
 @Repository
 public interface EventRepository extends MongoRepository<Event, String> {
 	
+	List<Event> findAllByOrderByCreatedAtDesc();
 	
+	Optional<Event> findTop1ByOrderIdOrderByCreatedAtDesc(String orderId);
+	
+	Optional<Event> findTop1ByTransactionIdOrderByCreatedAtDesc(String transactionalId);
 }
--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/service/EventService.java
@@ -1,12 +1,15 @@
 package br.com.microservices.orchestrated.orderservice.core.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.microservices.orchestrated.orderservice.config.exception.ValidationException;
 import br.com.microservices.orchestrated.orderservice.core.document.Event;
+import br.com.microservices.orchestrated.orderservice.core.dto.EventFilters;
 import br.com.microservices.orchestrated.orderservice.core.repository.EventRepository;
 
 @Service
@@ -32,4 +35,35 @@ public class EventService {
 		);
 	}
 	
+	public List<Event> findAll(){
+		return this.repository.findAllByOrderByCreatedAtDesc();
+	}
+	
+	public Event findByFilters(EventFilters filters) {
+		validateEmptyFilters(filters);
+		if(!filters.getOrderId().isEmpty()) {
+			return findByOrderId(filters.getOrderId());
+		}
+		else {
+			return findByTransactionId(filters.getTransactionalId());
+		}
+	}
+	
+	private void validateEmptyFilters(EventFilters filters) {
+		if(filters.getOrderId().isEmpty() && filters.getTransactionalId().isEmpty()) {
+			throw new ValidationException("OrderID or TransactionalID must be informed");
+		}
+	}
+	
+	private Event findByOrderId(String orderId) {
+		return this.repository.findTop1ByOrderIdOrderByCreatedAtDesc(orderId)
+			.orElseThrow(() -> new ValidationException("Evento not found by orderID"));
+	}
+	
+	private Event findByTransactionId(String transactionId) {
+		return this.repository.findTop1ByTransactionIdOrderByCreatedAtDesc(transactionId)
+			.orElseThrow(() -> new ValidationException("Evento not found by orderID"));
+	}
+	
 }
+  
