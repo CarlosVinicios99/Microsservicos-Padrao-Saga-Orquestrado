6c30316e49f8b010e2c3fd2c1e223c1ad2b1a327
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2026-04-09 13:23:03 -0300

    feature: criando classes de produto e de validação no serviço de validação de produto

---
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Product.java
@@ -0,0 +1,48 @@
+package br.com.microservices.orchestrated.productvalidationservice.core.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String code;
+	
+	
+	public Product() {
+		
+	}
+	
+	public Product(Integer id, String code) {
+		this.id = id;
+		this.code = code;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+}
+
