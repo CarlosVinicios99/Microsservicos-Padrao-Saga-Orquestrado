4b90f08d69c633ad25b9a8171ae0435eea3fbd7e
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2026-04-09 13:24:02 -0300

    feature: criando classes de produto e de validação no serviço de validação de produto

--- product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
+++ product-validation-service/src/main/java/br/com/microservices/orchestrated/productvalidationservice/core/model/Validation.java
@@ -1,5 +1,112 @@
 package br.com.microservices.orchestrated.productvalidationservice.core.model;
 
-public class Validation {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "validation")
+public class Validation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String orderId;
+	
+	@Column(nullable = false)
+	private String transactionId;
+	
+	@Column(nullable = false)
+	private boolean success;
+	
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+	
+	@Column(nullable = false)
+	private LocalDateTime updatedAt;
+	
+	
+	public Validation() {
+		
+	}
+
+	public Validation(Integer id, String orderId, String transactionId, boolean success, LocalDateTime createdAt,
+			LocalDateTime updatedAt) {
+		this.id = id;
+		this.orderId = orderId;
+		this.transactionId = transactionId;
+		this.success = success;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
+
+	public String getTransactionId() {
+		return transactionId;
+	}
+
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		LocalDateTime now = LocalDateTime.now();
+		this.createdAt = now;
+		this.updatedAt = now;
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		this.updatedAt = LocalDateTime.now();
+	}
+	
 }
