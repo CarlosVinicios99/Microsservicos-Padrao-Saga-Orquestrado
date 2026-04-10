b5f686a99655176dcd7f6cd140a5d7391e4b718d
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2026-04-10 13:24:21 -0300

    feature: criação do repositório para o serviço de pagamento

--- payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/repository/PaymentRepository.java
+++ payment-service/src/main/java/br/com/microservices/orchestrated/paymentservice/core/repository/PaymentRepository.java
@@ -1,13 +1,18 @@
 package br.com.microservices.orchestrated.paymentservice.core.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.microservices.orchestrated.paymentservice.core.model.Payment;
 
+
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer>{
 	
+	Boolean existsByOrderIdAndTransactionId(String orderId, String transactionId);
 	
+	Optional<Payment> findByOrderIdAndTransactionId(String orderId, String transactionId);
 
 }
