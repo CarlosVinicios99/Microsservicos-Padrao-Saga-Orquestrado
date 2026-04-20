052927acd9c8c8090f9c0fba30452bc52bcca3ab
Carlos Vinicios De Souza Azeredo <carlosvinicios@id.uff.br>
2026-04-20 16:55:25 -0300

    feature: criando readme

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Event.java
@@ -1,7 +1,6 @@
 package br.com.microservices.orchestrated.orderservice.core.document;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
